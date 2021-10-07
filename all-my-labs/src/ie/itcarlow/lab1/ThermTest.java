1b8d3118623fbb83aa54a0b9d2d6d40d43f3625b
C00262850 <C00262850@A313C012.itcarlow.ie>
2021-10-07 15:28:36 +0100

    Lab1

--- all-my-labs/src/ie/itcarlow/lab1/ThermTest.java
+++ all-my-labs/src/ie/itcarlow/lab1/ThermTest.java
@@ -1,8 +1,8 @@
 package ie.itcarlow.lab1;
 
-// Student Name 	: Oisin Cawley
-// Student Id Number: 
-// Date 			: Nov-2015
+// Student Name 	: Donavon Fox
+// Student Id Number: C00262850
+// Date 			: 07/10/2021
 // Purpose 			: My first class implementation
 
 public class ThermTest
@@ -11,10 +11,17 @@ public class ThermTest
 	{ // being main method
 
 		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+		
+		Thermometer thermB = new Thermometer(); //Creates the second instance for the lab question
 
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
+		///////////////////////////////////////////////////////////////////////////
+		thermB.setCelsius(10.0);
+		System.out.println("Temp. of Thermometer B is " + thermB.getCelsius() );//
+		
 		
 	} // end main
 } // end class ThermTest
