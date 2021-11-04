e15cb5c802459aa0c267a7afd336cadb986b10eb
donavonfox <91660839+donavonfox@users.noreply.github.com>
2021-11-04 15:43:07 +0000

    Lab5

---
+++ Circle.java
@@ -0,0 +1,23 @@
+package ie.itcarlow.lab5;
+
+public class Circle extends Point{
+
+	protected int radius;
+
+	public Circle (int x, int y, int radius) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	
+	
+	
+}
---
+++ MyFirstCircle.java
@@ -0,0 +1,27 @@
+package ie.itcarlow.lab5;
+
+public class MyFirstCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+
+
+
+			// Author : Donavon Fox 
+			// Date : 4/11/2021
+			// Purpose : A test driver program for our
+
+		
+			Point myPoint = new Point(10, 20);
+			Circle myCircle = new Circle(15, 30, 5);
+			System.out.println("Point details : " + 
+			myPoint);
+			System.out.println("Circle details: " + 
+			myCircle);
+			}
+			}	
+
+		
